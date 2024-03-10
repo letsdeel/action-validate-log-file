3f6febf740d0fb950d8108907bf8032073ee238c
Yariv Freifeld <yariv.freifeld@deel.com>
2024-03-10 10:46:24 +0200

    move to script

---
+++ validate.lock.file.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+registry=$1
+if [ -z $registry ] ; then
+  echo "missing registry argument"
+  exit -1
+fi
+FILES=$(jq -r '.packages[] | select(.resolved != null) | .resolved' package-lock.json | grep -v "https://npm-${registry}.d.codeartifact.eu-west-1.amazonaws.com")
+if [ $? != 0 ] ; then
+  echo "package-lock.json file is Ok"
+  exit 0
+else
+  IFS=" "
+  echo "wrong registries appear in package-lock.json file:"
+  echo $FILES
+  unset IFS
+fi
