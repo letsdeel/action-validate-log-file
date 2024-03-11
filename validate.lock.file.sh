e51a60a16b79a19007f68a2747174c4cc59cd316
Yariv Freifeld <yariv.freifeld@deel.com>
2024-03-11 17:11:44 +0200

    adding temporary exception for eslint

--- validate.lock.file.sh
+++ validate.lock.file.sh
@@ -5,7 +5,7 @@ if [ -z $registry ] ; then
   echo "missing registry argument"
   exit -1
 fi
-FILES=$(jq -r '.packages[] | select(.resolved != null) | .resolved' package-lock.json | grep -v "https://npm-${registry}.d.codeartifact.eu-west-1.amazonaws.com")
+FILES=$(jq -r '.packages[] | select(.resolved != null) | .resolved' package-lock.json | grep -v eslint | grep -v "https://npm-${registry}.d.codeartifact.eu-west-1.amazonaws.com")
 if [ $? != 0 ] ; then
   echo "package-lock.json file is Ok"
   exit 0
